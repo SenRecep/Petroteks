0b04d2a6bb7465c9a209b18741624681dd929105
recep <recep@DESKTOP-LN3BP89>
2019-12-26 23:44:05 +0300

    ilk kullaniciyi sisteme ekledik ve sifreleme ile ilgili adimlari gerceklestirdik

--- Petroteks.Entities/Concreate/User.cs
+++ Petroteks.Entities/Concreate/User.cs
@@ -8,14 +8,14 @@ namespace Petroteks.Entities.Concrete
     {
         public string Firstname { get; set; }
         public string Lastname { get; set; }
-        /*
-         * Admin:0;
-         * Editor:1;
-         * Standart:2;
-         */
         public string Email { get; set; }
         public string Password { get; set; }
         public string TagName { get; set; }
+        /// <summary>
+        ///  Admin:0
+        ///  Editor:1
+        ///  Standart:2
+        /// </summary>
         public short Role { get; set; } = 2;
 
     }

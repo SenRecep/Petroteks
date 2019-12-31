0fcfc2130fc460abe6be79d7745dd35b7df992bb
recep <recep@DESKTOP-LN3BP89>
2019-12-31 19:06:20 +0300

    dlls

--- Petroteks.Entities/Abstract/ICategory.cs
+++ Petroteks.Entities/Abstract/ICategory.cs
@@ -4,7 +4,10 @@ using System.Text;
 
 namespace Petroteks.Entities.Abstract
 {
-    interface ICategory
+    public interface ICategory
     {
+        int Parentid { get; set; }
+        string Name { get; set; }
+        string PhotoPath { get; set; }
     }
 }
---
+++ Petroteks.Entities/Concreate/Category.cs
@@ -0,0 +1,14 @@
+﻿using Petroteks.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Petroteks.Entities.Concreate
+{
+    public class Category : WebsiteObject, ICategory
+    {
+        public int Parentid { get; set; }
+        public string Name { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}

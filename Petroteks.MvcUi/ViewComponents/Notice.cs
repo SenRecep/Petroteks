e7fe9452fcd4c47cb131669201bb0ab4ae6e1620
Berat inceçam <berat639@hotmail.com>
2020-05-04 15:26:10 +0300

    View Duzeltme Vs

---
+++ Petroteks.MvcUi/ViewComponents/Notice.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Petroteks.Bll.Abstract;
+using Petroteks.Bll.Helpers;
+using Petroteks.Entities.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Petroteks.MvcUi.ViewComponents
+{
+    public class Notice : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        { 
+            return View();
+        }
+    }
+}
